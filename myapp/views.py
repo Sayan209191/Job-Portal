20f47a2f1d7b507f20cd8c26dc2741364e5de1f4
sayan209191 <sayanghorui611@gmail.com>
2024-12-03 22:37:50 +0530

    Home Page , Profile view & and Edit are updated

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from jobs.models import Job 
 
 # Create your views here.
 
 def index(request) :
-    return render(request, 'home/index.html')
+    # Fetch all jobs from the database
+    jobs = Job.objects.all()
+
+    # Pagination: Show 12 jobs per page
+    paginator = Paginator(jobs, 16)
+    page_number = request.GET.get('page', 1)  # Get the current page number from the request
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,  # Contains the paginated jobs
+    }
+    return render(request, 'home/index.html', context)
 
 def aboutus(request) :
     return render(request, 'footer/about.html')
