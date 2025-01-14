f7945158745d7de5efc976132b30e0f37b8cd38f
Sayan Ghorui <sayanghorui611@gmail.com>
2025-01-14 16:10:05 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
-    skills = models.TextField()  # Assume you handle this as a comma-separated list
+    skills = models.TextField()  #  handle this as a comma-separated list
     resume = models.FileField(upload_to='account/resumes/', blank=True, null=True)
     profile_picture = models.ImageField(upload_to='account/profilepicture/', blank=True, null=True)
     mobile_number = models.CharField(max_length=15, blank=True, null=True)
