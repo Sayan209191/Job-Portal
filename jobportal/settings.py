3164c3a8a62941132c2eb7a92767f3f7a0304029
sayan209191 <sayanghorui611@gmail.com>
2025-03-31 09:23:49 +0530

    Update database host in settings.py to new IP address

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'NAME': 'jobportal',
         'USER' : 'testuser',
         'PASSWORD' : 'Sayan@1234',
-        'HOST' : '192.168.254.202', #192.168.84.202 #192.168.146.202
+        'HOST' : '192.168.1.4', #192.168.84.202 #192.168.146.202
         'PORT' : '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
